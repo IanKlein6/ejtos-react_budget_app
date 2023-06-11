0c30f98e59bd02639501154c18297ae8450eb4e0
Ian Klein <ianklein66@gmail.com>
2023-06-11 15:45:37 +0200

    copy pasted budget into currency change as starting point

---
+++ src/components/CurrencyChange.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+	const { budget,dispatch,expenses } = useContext(AppContext);
+
+	const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		} 
+        else if(val>20000) {
+            alert("Budget cannot be greater than $20,000");
+        }
+        else {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: val,
+            })
+        }
+	}
+	
+	return (
+		<div className='alert alert-secondary'>
+            <span>Budget: </span>
+			<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+		</div>
+	);
+};
+
+export default Budget;
