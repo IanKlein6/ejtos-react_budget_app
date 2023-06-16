2b4d305d4a0f0c92beea16b957017c1bb88a17c4
Ian Klein <ianklein66@gmail.com>
2023-06-16 12:47:40 +0200

    created css, currency green color

--- src/components/CurrencyChange.js
+++ src/components/CurrencyChange.js
@@ -1,19 +1,29 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import "./StyleCurrency.css" ;
 
-const CurrencyChange = (props) => {
-	const { } = useContext(AppContext);
+const CurrencyChange = () => {
+  const {dispatch } = useContext(AppContext);
 
-    const [name, setName] = useState('');
-    const [action, setAction] = useState('');
-		
-
+	const changeCurrency = (val)=>{
+			dispatch({
+				type: 'CHG_CURRENCY',
+				payload: val,
+			})
+	}
 	
-	return (
-		<div className='alert alert-secondary'>
-            <span>Currencies: </span>
-			<input type="text"></input>
-		</div>
+   return (
+		<div className='alert alert-secondary' style={{backgroundColor: '#94DC99'}}>
+        <label style={{marginLeft: '.5rem' , backgroundColor:'#94DC99', color:'black'}} >Currency
+            <select name='hover_color' id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#FFC0CB', color:'white'}}>
+                <option style={{color:'black'}} value="£">(£ Pound)</option>
+                <option style={{color:'black'}} value="$">($ Dollar)</option>
+                <option style={{color:'black'}} value="€">(€ Euro)</option>
+                <option style={{color:'black'}} value="₹">(₹ Rupee)</option>
+            </select>	
+        </label>
+      	
+    </div>
 	);
 };
 
